e932435bd009d4e82ecf6a547596fdda62a32bb5
RossKeen <rosssamuelkeen@gmail.com>
2022-09-26 13:58:08 +0100

    add header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route></Route>
       </Routes>
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>NCGames</h1>
+      <p>
+        '<em>Life must be lived as play</em> ' - Plato
+      </p>
+    </header>
+  );
+};
+
+export default Header;
