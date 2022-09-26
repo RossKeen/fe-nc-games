8617529904c20f0085b9e2ea4205db97aa577dff
RossKeen <rosssamuelkeen@gmail.com>
2022-09-26 14:25:16 +0100

    add landing page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import "./App.css";
 import Footer from "./components/Footer";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Routes>
+        <Route path="/" element={<Landing />}></Route>
         <Route path="*" element={<p>404: The page you requested was not found...</p>}></Route>
       </Routes>
       <Footer />
---
+++ src/components/Landing.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section id="welcome">
+      <h2>Welcome to NCGames!</h2>
+      <p>
+        Tired of having only consonants in Scrabble? Had enough of catching your Nan stealing from the Monopoly bank during the 15th hour of the game? Can't face another impossible entertainment
+        question from your 1984 edition of Trivial Pursuit?
+      </p>
+      <p>Then look no further!</p>
+      <p>NCGames has many many obscure and niche boardgames and honest (sometimes a little too honest...) reviews to help you escape the Classics!</p>
+      <h3> New around here?</h3>
+      <p>Worry not! You are currently logged in as a guest and can browse, comment and post as you wish.</p>
+      <p>If you fancy a more personalised experience, you can create an account (feature in development).</p>
+    </section>
+  );
+};
+
+export default Landing;
