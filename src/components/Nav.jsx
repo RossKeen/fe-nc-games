0ecf7797e6612eb473bc3cf78a2b145e0bc22436
RossKeen <rosssamuelkeen@gmail.com>
2022-09-26 16:48:46 +0100

    add home link to navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav>
+      <Link className="link" to={"/"}>
+        Home
+      </Link>
       <Link className="link" to={"/reviews"}>
         Reviews
       </Link>
