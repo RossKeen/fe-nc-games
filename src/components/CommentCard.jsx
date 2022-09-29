b7210657435dac84233ff76920b81cc26da348e6
RossKeen <rosssamuelkeen@gmail.com>
2022-09-29 14:58:27 +0100

    add date parsing and styling for comment dates

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,8 +1,10 @@
+import { dateParser } from "../utils/utils";
+
 const CommentCard = ({ comment }) => {
   return (
     <li className="comment-card">
       <h3>{comment.author}</h3>
-      <p>{comment.created_at}</p>
+      <p className="posted-on">{`Posted on ${dateParser(comment.created_at)[0]} at ${dateParser(comment.created_at)[1]}`}</p>
       <p>{comment.body}</p>
     </li>
   );
